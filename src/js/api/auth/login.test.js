7f2f76cc92af55c7f1c70e8529ac496405586566
HMAsp <hanand55935@stud.noroff.no>
2023-11-03 12:43:11 +0100

    Adds login.test.js

---
+++ src/js/api/auth/login.test.js
@@ -0,0 +1,78 @@
+import { login } from "../auth/login.js";
+
+// Mock the global fetch function
+global.fetch = jest.fn(() =>
+  Promise.resolve({
+    ok: true,
+    json: () => Promise.resolve({ accessToken: "12345", user: "testUser" }),
+    statusText: "OK",
+  })
+);
+
+// Mocking localStorage
+const mockStorage = (() => {
+  let store = {};
+  return {
+    getItem(key) {
+      return store[key];
+    },
+    setItem(key, value) {
+      store[key] = value.toString();
+    },
+    clear() {
+      store = {};
+    },
+  };
+})();
+
+Object.defineProperty(global, "localStorage", { value: mockStorage });
+
+// Mocking the headers
+const headers = jest
+  .fn()
+  .mockReturnValue({ "Content-Type": "application/json" });
+
+// Mocking the api path
+jest.mock("./../constants.js", () => ({
+  apiPath: "/mockedApiPath",
+}));
+
+describe("login", () => {
+  beforeEach(() => {
+    fetch.mockClear();
+    headers.mockClear();
+    localStorage.clear();
+  });
+
+  it("Stores the token in localStorage", async () => {
+    const email = "testMail@example.com";
+    const password = "testPassword";
+
+    const profile = await login(email, password);
+
+    expect(fetch).toHaveBeenCalledTimes(1);
+    expect(fetch).toHaveBeenCalledWith("/mockedApiPath/social/auth/login", {
+      method: "post",
+      headers: headers(),
+      body: JSON.stringify({ email, password }),
+    });
+
+    const token = JSON.parse(localStorage.getItem("token"));
+    expect(token).toBe("12345");
+
+    expect(profile.accessToken).toBeUndefined();
+  });
+
+  it("throws an error if the response is not ok", async () => {
+    fetch.mockImplementationOnce(() =>
+      Promise.resolve({
+        ok: false,
+        statusText: "Unauthorized",
+      })
+    );
+
+    await expect(login("testMail@example.com", "testPassword")).rejects.toThrow(
+      "Unauthorized"
+    );
+  });
+});
